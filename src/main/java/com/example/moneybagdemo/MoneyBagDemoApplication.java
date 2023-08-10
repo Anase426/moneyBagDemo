54d591180c5ae306b0591bed5e2b37c103359334
Anase426 <69397210+Anase426@users.noreply.github.com>
2023-08-10 18:42:50 +0800

    1. 查询用户钱包余额 2. 用户消费100元的接口 3. 用户退款20元接口 4. 查询用户钱包金额变动明细的接口

---
+++ src/main/java/com/example/moneybagdemo/MoneyBagDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.moneybagdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyBagDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyBagDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moneybagdemo/MoneyBagDemoApplicationTests.java
@@ -0,0 +1,74 @@
+package com.example.moneybagdemo;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@SpringBootTest
+class MoneyBagDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        FastAutoGenerator.create("jdbc:mysql://localhost:3306/moneybagdemo?serverTimezone=UTC&characterEncoding=utf8&useUnicode=true&useSSL=false",
+                        "root", "123456")
+                .globalConfig(builder -> {
+                    builder.author("linzecheng") // 设置作者
+                            .enableSwagger() // 开启 swagger 模式
+                            .fileOverride() // 覆盖已生成文件
+                            .outputDir(System.getProperty("user.dir")+"\\src\\main\\java"); // 指定输出目录
+                })
+                .dataSourceConfig(builder -> builder.typeConvertHandler((globalConfig, typeRegistry, metaInfo) -> {
+                    int typeCode = metaInfo.getJdbcType().TYPE_CODE;
+                    if (typeCode == Types.SMALLINT) {
+                        // 自定义类型转换
+                        return DbColumnType.INTEGER;
+                    }
+                    return typeRegistry.getColumnType(metaInfo);
+
+                }))
+                .packageConfig(builder -> {
+                    builder.parent("com.example") // 设置父包名
+                            .moduleName("moneybagdemo") // 设置父包模块名
+                            .service("service")
+                            .serviceImpl("service.impl")
+                            .controller("controller")
+                            .mapper("mapper")
+                            .xml("mapper")
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, System.getProperty("user.dir")+"\\src\\main\\resources\\mapper")); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    List<String> tables=new ArrayList<>();
+                    tables.add("moneybag");
+                    tables.add("moneybag_bill");
+                    builder.addInclude(tables)
+                            .serviceBuilder()
+                            .formatServiceFileName("%sService")
+                            .formatServiceImplFileName("%sServiceImpl")
+                            .entityBuilder()
+                            .enableLombok()
+                            .logicDeleteColumnName("deleted")
+                            .enableTableFieldAnnotation()
+                            .controllerBuilder()
+                            .formatFileName("%sController")
+                            .enableRestStyle()
+                            .mapperBuilder()
+                            //生成通用的resultMap
+                            .enableBaseResultMap()
+                            .superClass(BaseMapper.class)
+                            .formatMapperFileName("%sMapper")
+                            .enableMapperAnnotation()
+                            .formatXmlFileName("%sMapper");
+                })
+                .execute();
+    }
+
+}
